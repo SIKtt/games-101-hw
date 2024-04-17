ec60e27fa98742491b69227701e102a905d16f23
siqin <me@siqinsyi.com>
2024-04-17 21:03:02 +0800

    remove cmdline

--- main.cpp
+++ main.cpp
@@ -65,6 +65,45 @@ Eigen::Matrix4f get_model_matrix(float rotation_angle, int axis)
     return model;
 }
 
+Eigen::Matrix4f change_model_matrix(Eigen::Matrix4f model_matrix, float rotation_angle, int axis)
+{
+
+    float cos_phi = std::cos(rotation_angle);
+    float sin_phi = std::sin(rotation_angle);
+
+    Eigen::Matrix4f rotation;
+    if (axis==0) // x-axis
+    {
+    rotation << 
+        1, 0, 0, 0,
+        0, cos_phi, -sin_phi, 0,
+        0, sin_phi, cos_phi, 0,
+        0, 0, 0, 1;
+    }
+    if (axis==1)
+    {
+    rotation << 
+        cos_phi, 0, sin_phi, 0,
+        0, 1, 0, 0,
+        -sin_phi, 0, cos_phi, 0,
+        0, 0, 0, 1;
+    }
+    if (axis==2) // z-axis
+    {
+    rotation << 
+        cos_phi, -sin_phi, 0, 0, 
+        sin_phi, cos_phi, 0, 0, 
+        0, 0, 1, 0,
+        0, 0, 0, 1;
+    }
+
+
+    model = rotation * model_matrix;
+
+    return model;
+}
+
+
 Eigen::Matrix4f get_projection_matrix(float eye_fov, float aspect_ratio,
                                       float zNear, float zFar)
 {
@@ -103,19 +142,11 @@ int main(int argc, const char** argv)
     bool command_line = false;
     std::string filename = "output.png";
 
-    if (argc >= 3) {
-        command_line = true;
-        angle = std::stof(argv[2]); // -r by default
-        if (argc == 4) {
-            filename = std::string(argv[3]);
-        }
-        else
-            return 0;
-    }
-
     rst::rasterizer r(700, 700);
 
     Eigen::Vector3f eye_pos = {0, 0, 10};
+
+    Eigen::Matrix4f model_pos = Eigen::Matrix4f::Identity();
 
     std::vector<Eigen::Vector3f> pos{{2, 0, -2}, {0, 2, -2}, {-2, 0, -2}};
 
@@ -126,22 +157,6 @@ int main(int argc, const char** argv)
 
     int key = 0;
     int frame_count = 0;
-
-    if (command_line) {
-        r.clear(rst::Buffers::Color | rst::Buffers::Depth);
-
-        r.set_model(get_model_matrix(angle, axis));
-        r.set_view(get_view_matrix(eye_pos));
-        r.set_projection(get_projection_matrix(45, 1, 0.1, 50));
-
-        r.draw(pos_id, ind_id, rst::Primitive::Triangle);
-        cv::Mat image(700, 700, CV_32FC3, r.frame_buffer().data());
-        image.convertTo(image, CV_8UC3, 1.0f);
-
-        cv::imwrite(filename, image);
-
-        return 0;
-    }
 
     while (key != 27) {
         r.clear(rst::Buffers::Color | rst::Buffers::Depth);
